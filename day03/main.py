bc4014ceff4199c6e84d1ff6852536ed522b5a0f
Klas Eskilson <klas@billogram.com>
2020-12-03 14:01:54 +0100

    last golfing for day 3

--- day03/main.py
+++ day03/main.py
@@ -1,4 +1,4 @@
-def count(trees, dy: int, dx: int) -> int:
+def count(trees: [str], dy: int, dx: int) -> int:
     c, x, y = [0, 0, 0]
     while y < len(trees):
         c += trees[y][x % len(trees[0])] == "#"
@@ -9,10 +9,13 @@ def count(trees, dy: int, dx: int) -> int:
 
 def main():
     inputs = [str(s) for s in open("input.txt", "r").read().splitlines()]
+    print("part one:", count(inputs, 1, 3))
+
+    # part 2
     moves = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
     a = 1
     [a := a * c for c in [count(inputs, *move) for move in moves]]
-    print(a)
+    print("part two:", a)
 
 
 if __name__ == "__main__":
