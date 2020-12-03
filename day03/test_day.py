50fcf3fb6095dda359046399ada3ba68af9ed975
Klas Eskilson <klas@billogram.com>
2020-12-03 14:17:49 +0100

    clean up test of day 3

--- day03/test_day.py
+++ day03/test_day.py
@@ -1,8 +1,8 @@
 import pytest
 
-from main import Map
+from main import count
 
-M = """..##.......
+TREES = """..##.......
 #...#...#..
 .#....#..#.
 ..#.#...#.#
@@ -12,40 +12,8 @@ M = """..##.......
 .#........#
 #.##...#...
 #...##....#
-.#..#...#.#
-"""
-
-
-# def test_map_print():
-#     t = Map.from_input(M)
-#     assert t.p() == str(M)
-
-
-def test_map_size():
-    t = Map.from_input(M)
-    assert t.size() == [11, 11]
+.#..#...#.#"""
 
 
 def test_count():
-    t = Map.from_input(M)
-    moves = 10
-    c = 0
-    for i in range(moves):
-        t.move()
-        if t.curr() == "#":
-            c += 1
-
-    assert c == 7
-
-
-def test_map_moving():
-    t = Map.from_input(M)
-    assert t.curr() == "."
-    t.move()
-    assert t.curr() == "."
-    t.move()
-    assert t.curr() == "#"
-    t.move()
-    assert t.curr() == "."
-    t.move()
-    assert t.curr() == "#"
+    assert count(TREES.split("\n"), 1, 3) == 7
