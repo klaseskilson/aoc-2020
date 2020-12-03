7c902c6e380c21e4a70d59e284586508d113a52d
Klas Eskilson <klas@billogram.com>
2020-12-03 13:33:56 +0100

    intensify golfing in day 3

--- day03/main.py
+++ day03/main.py
@@ -8,17 +8,13 @@ MOVE_ROW = 1
 
 
 @dataclass
-class Map:
+class Trees:
     m: [str]
     pos: [int]
 
     def __init__(self, tree):
         self.m = tree
         self.pos = [0, 0]
-
-    @classmethod
-    def from_input(cls, inpu: bytearray):
-        return cls([str(s) for s in inpu.splitlines()])
 
     def at(self, pos: [int]) -> str:
         row, col = pos
@@ -27,40 +23,27 @@ class Map:
     def curr(self) -> str:
         return self.at(self.pos)
 
-    def move(self, mr: int = MOVE_ROW, mc: int = MOVE_COL) -> bool:
+    def move(self, mr: int = MOVE_ROW, dx: int = MOVE_COL) -> bool:
         row, col = self.pos
         nr = row + mr
-        nc = col + mc
         if nr >= len(self.m):
             return False
-        if nc >= len(self.m[0]):
-            nc = nc % len(self.m[0])
-        self.pos = [nr, nc]
+        self.pos = [nr, (col + dx) % len(self.m[0])]
         return True
 
-    def size(self) -> [int]:
-        return [len(self.m), len(self.m[0])]
-
-    def p(self) -> str:
-        return "\n".join(self.m)
+    def count(self, dy: int, dx: int) -> int:
+        c = 0
+        while self.move(dy, dx):
+            c += int(self.curr() == "#")
+        return c
 
 
 def main():
-    inputs = open("input.txt", "r").read()
-    m = [[1,1], [1,3], [1, 5], [1, 7], [2, 1]]
+    inputs = [str(s) for s in open("input.txt", "r").read().splitlines()]
+    moves = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
     a = 1
-
-    for move in m:
-        trees = Map.from_input(inputs)
-        c = 0
-        while trees.move(*move):
-            if trees.curr() == "#":
-                c += 1
-        a *= c
-        print(c)
-        print(a)
-
-    print(c)
+    [a := a * c for c in [Trees(inputs).count(*move) for move in moves]]
+    print(a)
 
 
 if __name__ == "__main__":
