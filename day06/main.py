7b824be486be3db3f035140fa5a1cc623cc4a8fd
Klas Eskilson <klas@billogram.com>
2020-12-06 17:08:33 +0100

    add day 6 part 2

--- day06/main.py
+++ day06/main.py
@@ -1,14 +1,15 @@
 def count(s: str) -> int:
-    uniq = set()
-    for l in s.replace("\n", ""):
-        uniq.add(l)
-    return len(uniq)
+    return len(set(s.replace("\n", "")))
+
+
+def count_unified(s: str) -> int:
+    return len(set(s[0]).intersection(*[set(s) for s in s[1:]]))
 
 
 def main():
-    answers = [str(s) for s in open("input.txt", "r").read().split("\n\n")]
-    groups = [count(s) for s in answers]
-    print("part 1", sum(groups))
+    answers = open("input.txt", "r").read().split("\n\n")
+    print("part 1", sum([count(s) for s in answers]))
+    print("part 2", sum([count_unified(s.split('\n')) for s in answers]))
 
 
 if __name__ == "__main__":
