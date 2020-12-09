397b4379c1126596aec317f10fea4d67ea271184
Klas Eskilson <klas@billogram.com>
2020-12-09 20:42:35 +0100

    add day 8 part 2

--- day08/main.py
+++ day08/main.py
@@ -1,9 +1,9 @@
-def calc(inp: [[str, int]]) -> int:
+def calc(inp: [[str, int]]) -> [int, bool]:
     acc = 0
     i = 0
     seen = []
     while True:
-        if i in seen:
+        if i in seen or i == len(inp):
             break
         seen.append(i)
         inst, val = inp[i]
@@ -23,7 +23,22 @@ def calc(inp: [[str, int]]) -> int:
 def main():
     inp = open("input.txt", "r").read().splitlines()
     instr = [[a.split(' ')[0], int(a.split(' ')[1])] for a in inp]
-    print("part 1", calc(instr))
+    final, success = calc(instr)
+    print("part 1", final, success)
+
+    ## part 2
+    for i in range(len(instr)):
+        cmd, _val = instr[i]
+        cp = [ele[:] for ele in instr]
+        if cmd == 'nop':
+            cp[i][0] = 'jmp'
+        elif cmd == 'jmp':
+            cp[i][0] = 'nop'
+        f, s = calc(cp)
+        if s:
+            print("part 2", f, s, i, cmd)
+            break
+
 
 if __name__ == "__main__":
     main()
